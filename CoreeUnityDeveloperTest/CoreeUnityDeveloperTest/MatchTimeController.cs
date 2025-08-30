facd698bb8dd7e916e423cfb4a2d8ca074cf64f5
Sobhan_11 <83942874+sobhan-11@users.noreply.github.com>
2025-08-30 17:49:37 +0300

    Add Initial Files and Scripts

---
+++ CoreeUnityDeveloperTest/CoreeUnityDeveloperTest/MatchTimeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CoreeUnityDeveloperTest
+{
+    public class MatchTimeController
+    {
+        public int currentTimeFrame; // * Bonus * : Improve this line!
+
+        public void StartGameUpdate()
+        {
+            try
+            {
+                MatchUpdate();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        // * Issue * : write your main code logic handle current time frame here!
+        private void MatchUpdate()
+        {
+            
+        }
+    }
+}
